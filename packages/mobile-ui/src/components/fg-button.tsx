474d191b2bc63903cb790cb6e60d30e4fa086b9e
psilva1982 <severos1982@gmail.com>
2026-05-20 10:48:12 -0300

    feat: refactor FGButton component to enhance functionality and structure

--- packages/mobile-ui/src/components/fg-button.tsx
+++ packages/mobile-ui/src/components/fg-button.tsx
@@ -1,3 +1,33 @@
-import { Button as FGButton } from "../../components/ui/button";
+import { ComponentProps } from "react";
 
-export { FGButton };
+import {
+  Button as GluestackButton,
+  ButtonSpinner,
+  ButtonText,
+} from "../../components/ui/button";
+
+type Props = ComponentProps<typeof GluestackButton> & {
+  label: string;
+  variant?: "solid" | "outline";
+  isLoading?: boolean;
+  block?: boolean;
+};
+
+export function FGButton({
+  label,
+  isLoading = false,
+  block = false,
+  variant = "solid",
+  ...rest
+}: Props) {
+  return (
+    <GluestackButton
+      variant={variant}
+      className={`${block ? "w-full" : "w-fit"} items-center gap-3`}
+      {...rest}
+    >
+      {isLoading && <ButtonSpinner color="white" />}
+      <ButtonText className="text-primary-0">{label}</ButtonText>
+    </GluestackButton>
+  );
+}
